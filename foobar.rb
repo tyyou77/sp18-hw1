204202fbd50057bdc60f380d68e2b2c5695fea90
Charles You <cyou77@berkeley.edu>
2018-02-15 15:06:45 -0800

    Done

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,9 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    temp = a.map{|x| x.to_i + 2}
+    temp2 = temp.select{|x| x % 2 == 0}.uniq
+    result = temp2.select{|x| x < 10}
+    result.reduce(0, :+)
   end
 end
